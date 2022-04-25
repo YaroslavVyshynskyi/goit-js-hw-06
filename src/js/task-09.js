60bf117dee8f0e6665f55ed4b534efacb864530e
Yaroslav Vyshynskyi <92641328+YaroslavVyshynskyi@users.noreply.github.com>
2022-04-25 15:34:55 +0300

    task 9 - done

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const div = document.querySelector(".widget");
+const span = document.querySelector(".color");
+const btn = document.querySelector(".change-color");
+
+btn.addEventListener('click', changeColor);
+
+function changeColor() { 
+  const color = getRandomHexColor();
+  div.style.backgroundColor = color;
+  span.innerHTML = color;
+};
